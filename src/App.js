07f01781549a19373470bfde68a234a2bdfc28ef
Henry <you@example.com>
2022-03-14 17:13:01 -0600

    refactoring fetch function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React, {useEffect} from 'react'
 import Header from './components/Header'
 import './App.css';
 
-fetch("https://search.torre.co/people/_search/?",{method: "POST"})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+// fetch("https://search.torre.co/people/_search/?",{method: "POST"})
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
 
 function App() {
+
+  const url = "https://search.torre.co/people/_search/?";
+
+  const fetchPeople = () => {
+    fetch(url,{method: 'POST'})
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+  };
+
+  useEffect(() => {
+    fetchPeople(url);
+  }, [])
+
   return (
     <div className="container">
         <Header brand="Torre's Candidate Index" />
